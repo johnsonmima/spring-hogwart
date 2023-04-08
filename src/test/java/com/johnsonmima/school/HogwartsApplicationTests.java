6652e05f7dde9c90073a7c60f9b8a05964084a10
Johnson Olusegun <olusegun.johnson.oj@gmail.com>
2023-04-08 00:54:19 -0400

    Demo of SpringBoot Autoconfiguration

---
+++ src/test/java/com/johnsonmima/school/HogwartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnsonmima.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
