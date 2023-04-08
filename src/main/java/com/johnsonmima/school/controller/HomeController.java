f1a83d8d6b8bf548f45a99bb2001258e565e8536
Johnson Olusegun <olusegun.johnson.oj@gmail.com>
2023-04-08 16:12:26 -0400

    make use of bootstrap starter template

--- src/main/java/com/johnsonmima/school/controller/HomeController.java
+++ src/main/java/com/johnsonmima/school/controller/HomeController.java
@@ -1,13 +1,14 @@
 package com.johnsonmima.school.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
+    @RequestMapping(value = {"","/","/home"})
     public String displayHomePage(){
         return "home.html";
     }
